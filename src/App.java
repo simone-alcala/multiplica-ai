7a115270deb0354ce9130fe9df129d9edaf19196
simone.alcala <simone.alcala@gmail.com>
2023-01-09 21:15:09 -0300

    Feat: Implement multiplication table

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.print("Digite um número inteiro: ");
+            int number = scanner.nextInt();
+            for (int i = 1; i <= 10; i++) {
+                int result = number * i;
+                System.out.print(number + " x " + i + " = " + result + "\n");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Número inválido =/");
+        } catch (Exception e) {
+            System.out.println("Erro não detectado... oO");
+        } finally {
+            scanner.close();
+        }
     }
+
 }
