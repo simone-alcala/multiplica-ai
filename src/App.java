8b614ea83ae56b95598cdd51abc02f8328356b11
simone.alcala <simone.alcala@gmail.com>
2023-01-09 21:20:03 -0300

    Fix: Show multiplication table

--- src/App.java
+++ src/App.java
@@ -8,10 +8,14 @@ public class App {
         try {
             System.out.print("Digite um número inteiro: ");
             int number = scanner.nextInt();
+            System.out.println("------------------------------");
+            System.out.println("Tabela de multiplicação de " + number);
+            System.out.println("------------------------------");
             for (int i = 1; i <= 10; i++) {
                 int result = number * i;
                 System.out.print(number + " x " + i + " = " + result + "\n");
             }
+            System.out.println("------------------------------");
         } catch (InputMismatchException e) {
             System.out.println("Número inválido =/");
         } catch (Exception e) {
